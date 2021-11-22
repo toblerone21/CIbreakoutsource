a4efeb84b836c034917ad94f734671a9935c93b2
Ashlocken <92615800+Ashlocken@users.noreply.github.com>
2021-11-22 17:14:43 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
-/*module.exports = {
-    mongoURI: 'mongodb+srv://Breakout:<password>@cibreakout.2kxfc.mongodb.net/Authenticate?retryWrites=true&w=majority'
-}*/
+
 
 
 dbPassword = 'mongodb://Breakout:JyJWAFVfjFXnDB1f@cibreakout-shard-00-00.2kxfc.mongodb.net:27017,cibreakout-shard-00-01.2kxfc.mongodb.net:27017,cibreakout-shard-00-02.2kxfc.mongodb.net:27017/Test?ssl=true&replicaSet=atlas-uldhkg-shard-0&authSource=admin&retryWrites=true&w=majority';

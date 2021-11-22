9c208472a98e4a9ad524fd9bca424728961951db
Matthew Harris <Matt25373@gmail.com>
2021-11-22 17:23:13 -0500

    22nd

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,3 @@
-/*module.exports = {
-    mongoURI: 'mongodb+srv://Breakout:<password>@cibreakout.2kxfc.mongodb.net/Authenticate?retryWrites=true&w=majority'
-}*/
 
 
 dbPassword = 'mongodb://Breakout:<password>@cibreakout-shard-00-00.2kxfc.mongodb.net:27017,cibreakout-shard-00-01.2kxfc.mongodb.net:27017,cibreakout-shard-00-02.2kxfc.mongodb.net:27017/Test?ssl=true&replicaSet=atlas-uldhkg-shard-0&authSource=admin&retryWrites=true&w=majority';
